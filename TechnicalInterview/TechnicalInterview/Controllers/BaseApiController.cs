b2f8e8fa8f18b833f21127e0abc80407f8463f58
cabrera06 <edcsandi@gmail.com>
2025-12-04 17:20:27 -0600

    Clean Architecture

---
+++ TechnicalInterview/TechnicalInterview/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechnicalInterview.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
